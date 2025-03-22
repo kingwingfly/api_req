60ae0feea2fce9cba0fbcec5c10d082be478904b
Louis <836250617@qq.com>
2025-03-23 03:28:23 +0800

    Add repository field and README files
    - Added repository URL to Cargo.toml files
    - Created LICENSE file with MIT License
    - Added README.md files and included them in the library documentation

--- api_req_derive/src/lib.rs
+++ api_req_derive/src/lib.rs
@@ -1,5 +1,4 @@
-//! Derive macros for hyperswitch_utils
-
+#![doc = include_str!("../README.md")]
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
